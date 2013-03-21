2fffbdd00638cf8fb975feedb5e6717fcac82b07
captconrado <ottey001@gmail.com>
2013-03-21 15:08:50 -0700

    added geocoding

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :comment, :date, :location, :title
+  attr_accessible :comment, :date, :location, :title, :address,:latitude, :longitude
+  geocoded_by :location
+  after_validation :geocode
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130321204912) do
+ActiveRecord::Schema.define(:version => 20130321220618) do
 
   create_table "events", :force => true do |t|
     t.string   "date"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(:version => 20130321204912) do
     t.string   "comment"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
