d074fe7ee54fb02c41eced45f7db64870f14665d
captconrado <ottey001@gmail.com>
2013-03-21 15:18:01 -0700

    ran annotate

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :integer          not null, primary key
+#  date       :string(255)
+#  title      :string(255)
+#  location   :string(255)
+#  comment    :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  latitude   :float
+#  longitude  :float
+#
+
 class Event < ActiveRecord::Base
   attr_accessible :comment, :date, :location, :title, :address,:latitude, :longitude
   geocoded_by :location
